e292b5c900710169c368a27e76ba67249c9679af
Stanley Yuen <ypang.yuen@gmail.com>
2019-04-23 00:41:19 +0800

    Update config.js
    Update

--- frontend/dist/config.js
+++ frontend/dist/config.js
@@ -2,16 +2,16 @@ var awsConfig = {
     API:{
         endpoints: [
             {
-                name: "RestAPI",
-                endpoint: "https://o6mr4aunn9.execute-api.ap-southeast-1.amazonaws.com/v1",
-                region: "ap-southeast-1"
+                name: "{ApiId}",
+                endpoint: "{ApiBaseUrl}",
+                region: "{Region}"
             }
         ]
     },
     Auth:{
-        identityPoolId: "ap-southeast-1:b4436e22-426b-48df-b327-ccd93a201e6b", 
-        userPoolId: "ap-southeast-1_gpKTFKSqa",
-        UserPoolWebClientId: "6qh50r5i0ljmln4kqlaki5l3jo",
-        region: "ap-southeast-1"
+        identityPoolId: "{IdPoolId}", 
+        userPoolId: "{UserPoolId}",
+        UserPoolWebClientId: "{UserPoolWebClientId}",
+        region: "{Region}"
     }
 }
